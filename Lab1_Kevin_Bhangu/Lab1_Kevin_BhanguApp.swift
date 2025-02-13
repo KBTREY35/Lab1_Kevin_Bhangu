9d9ceda905aa0e563fddc384142094833ca8cd15
Kevin <101418717@georgebrown.ca>
2025-02-13 18:02:12 -0500

    Initial Commit

---
+++ Lab1_Kevin_Bhangu/Lab1_Kevin_BhanguApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab1_Kevin_BhanguApp.swift
+//  Lab1_Kevin_Bhangu
+//
+//  Created by kevin bhangu on 2025-02-13.
+//
+
+import SwiftUI
+
+@main
+struct Lab1_Kevin_BhanguApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
